4c91651dd16fd78a811ef596f0e4c9ac3eec725d
Marcus Pöhls <marcus.poehls@gmail.com>
2020-08-05 10:16:39 +0200

    better map

--- src/map.ts
+++ src/map.ts
@@ -1,6 +1,22 @@
 'use strict'
 
-export class SuperchargedMap<K, V> extends Map<K, V> {
+import { tap } from '@supercharge/goodies'
+
+export class SuperchargedMap<K, V> implements Iterable<[K, V]> {
+  /**
+   * Contains the key-value-pairs in the map.
+   */
+  private readonly items: Map<K, V>
+
+  /**
+   * Create a new map instance for the given `entries`.
+   *
+   * @param entries
+   */
+  constructor (entries?: Array<[K, V]> | null) {
+    this.items = new Map<K, V>(entries)
+  }
+
   /**
    * Create a new map instance of the given key/value pair `entries`.
    *
@@ -8,26 +24,71 @@ export class SuperchargedMap<K, V> extends Map<K, V> {
    *
    * @returns {SuperchargedMap}
    */
-  static of<K, V> (entries?: Array<[K, V]> | null): SuperchargedMap<K, V> {
+  static of<K, V> (entries?: Array<[K, V]>): SuperchargedMap<K, V> {
     return new this<K, V>(entries)
   }
 
   /**
-   * Determine whether the map is empty (contains no entries).
+   * Returns an iterable of key-value-pairs for every entry in the map.
    *
-   * @returns {Boolean}
+   * @returns {IterableIterator}
    */
-  isEmpty (): boolean {
-    return this.size === 0
+  [Symbol.iterator] (): IterableIterator<[K, V]> {
+    return this.entries()
   }
 
   /**
-   * Determine whether the map is not empty (contains entries).
+   * Clears the map by removing all entries.
+   *
+   * @returns {SuperchargedMap}
+   */
+  clear (): this {
+    return tap(this, () => {
+      this.items.clear()
+    })
+  }
+
+  /**
+   * Delete the given `value` from the set.
+   *
+   * @param {*} key
    *
    * @returns {Boolean}
    */
-  isNotEmpty (): boolean {
-    return !this.isEmpty()
+  delete (key: K): boolean {
+    return this.items.delete(key)
+  }
+
+  /**
+   * Returns an iterable of key-value-pairs for every entry in the map.
+   *
+   * @returns {IterableIterator}
+   */
+  entries (): IterableIterator<[K, V]> {
+    return this.items.entries()
+  }
+
+  /**
+   * Runs the given `action` on each key-value-pair in the set.
+   *
+   * @param {Function} action
+   */
+  forEach (action: (key: K, value: V, set: SuperchargedMap<K, V>) => void): void {
+    this.items.forEach((value: V, key: K) => {
+      action(key, value, this)
+    })
+  }
+
+  /**
+   * Returns the value stored for the the given `key`. Returns
+   * `undefined` if the `key` is not present in the map.
+   *
+   * @param key
+   *
+   * @returns {*}
+   */
+  get (key: K): V | undefined {
+    return this.items.get(key)
   }
 
   /**
@@ -44,6 +105,77 @@ export class SuperchargedMap<K, V> extends Map<K, V> {
   }
 
   /**
+   * Determine whether the set contains the given `value`.
+   *
+   * @param {*} value
+   *
+   * @returns {Boolean}
+   */
+  has (key: K): boolean {
+    return this.items.has(key)
+  }
+
+  /**
+   * Determine whether the map is empty (contains no entries).
+   *
+   * @returns {Boolean}
+   */
+  isEmpty (): boolean {
+    return this.size() === 0
+  }
+
+  /**
+   * Determine whether the map is not empty (contains entries).
+   *
+   * @returns {Boolean}
+   */
+  isNotEmpty (): boolean {
+    return !this.isEmpty()
+  }
+
+  /**
+   * Returns an iterable of the map keys.
+   *
+   * @returns {IterableIterator}
+   */
+  keys (): IterableIterator<K> {
+    return this.items.keys()
+  }
+
+  /**
+   * Set the `key`-`value`-pair in the map. This method overrides
+   * a map entry with the given `key` if it already exists.
+   *
+   * @param {*} key
+   * @param {*} value
+   *
+   * @returns {SuperchargedMap}
+   */
+  set (key: K, value: V): this {
+    return tap(this, () => {
+      this.items.set(key, value)
+    })
+  }
+
+  /**
+   * Returns the size of the map.
+   *
+   * @returns {Number}
+   */
+  size (): number {
+    return this.items.size
+  }
+
+  /**
+   * Returns an iterable of the map values.
+   *
+   * @returns {IterableIterator}
+   */
+  values (): IterableIterator<V> {
+    return this.items.values()
+  }
+
+  /**
    * Returns an array containing the results of applying the
    * given `transform` function to each entry in the map.
    *
@@ -51,11 +183,33 @@ export class SuperchargedMap<K, V> extends Map<K, V> {
    *
    * @returns {Array}
    */
-  map<R> (transform: (value: V, key: K, map: SuperchargedMap<K, V>) => R): R[] {
+  map<R> (transform: (key: K, value: V, map: SuperchargedMap<K, V>) => R): R[] {
     const results: R[] = []
 
-    this.forEach((value, key) => {
-      results.push(transform(value, key, this))
+    this.forEach((key, value) => {
+      results.push(
+        transform(key, value, this)
+      )
+    })
+
+    return results
+  }
+
+  /**
+   * Returns an array containing the results of applying the
+   * given `transform` function to each entry in the map.
+   *
+   * @param {Function} transform
+   *
+   * @returns {Array}
+   */
+  filter (predicate: (key: K, value: V, map: SuperchargedMap<K, V>) => boolean): SuperchargedMap<K, V> {
+    const results: SuperchargedMap<K, V> = new SuperchargedMap<K, V>()
+
+    this.forEach((key: K, value: V) => {
+      if (predicate(key, value, this)) {
+        results.set(key, value)
+      }
     })
 
     return results
--- test/map.js
+++ test/map.js
@@ -20,6 +20,51 @@ describe('Maps', () => {
     expect(Map.of([['key', 'value'], ['name', 'Marcus']]).has('key')).to.be.true()
   })
 
+  it('clear', () => {
+    const map = Map.of([
+      [1, 'Marcus'],
+      [2, 'Supercharge']
+    ])
+
+    expect(map.size()).to.equal(2)
+    expect(map.has(1)).to.be.true()
+    expect(map.has(2)).to.be.true()
+
+    map.clear()
+
+    expect(map.size()).to.equal(0)
+    expect(map.has(1)).to.be.false()
+    expect(map.has(2)).to.be.false()
+  })
+
+  it('delete', () => {
+    const map = Map.of([
+      [1, 'Marcus'],
+      [2, 'Supercharge']
+    ])
+
+    expect(map.size()).to.equal(2)
+    expect(map.has(1)).to.be.true()
+
+    map.delete(1)
+
+    expect(map.size()).to.equal(1)
+    expect(map.has(1)).to.be.false()
+  })
+
+  it('entries', () => {
+    const map = Map.of([
+      [1, 'Marcus'],
+      [2, 'Supercharge']
+    ])
+
+    const iterator = map.entries()
+
+    expect(iterator.next().value).to.equal([1, 'Marcus'])
+    expect(iterator.next().value).to.equal([2, 'Supercharge'])
+    expect(iterator.next().value).to.be.undefined()
+  })
+
   it('isEmpty', () => {
     const map = new Map()
     expect(map.isEmpty()).to.be.true()
@@ -31,6 +76,19 @@ describe('Maps', () => {
 
     map.set('name', 'Marcus')
     expect(map.isNotEmpty()).to.be.true()
+  })
+
+  it('keys', () => {
+    const map = Map.of([
+      [1, 'Marcus'],
+      [2, 'Supercharge']
+    ])
+
+    const iterator = map.keys()
+
+    expect(iterator.next().value).to.equal(1)
+    expect(iterator.next().value).to.equal(2)
+    expect(iterator.next().value).to.be.undefined()
   })
 
   it('getOrDefault', () => {
@@ -54,13 +112,85 @@ describe('Maps', () => {
       .set('user:1', 'Marcus')
       .set('user:2', 'Supercharge')
 
-    const users = cache.map((value, key) => {
+    const users = cache.map((key, value) => {
       return { [key]: value }
     })
 
     expect(users).to.equal([
       { 'user:1': 'Marcus' },
       { 'user:2': 'Supercharge' }
+    ])
+  })
+
+  it('values', () => {
+    const map = Map.of([
+      [1, 'Marcus'],
+      [2, 'Supercharge']
+    ])
+
+    const iterator = map.values()
+
+    expect(iterator.next().value).to.equal('Marcus')
+    expect(iterator.next().value).to.equal('Supercharge')
+    expect(iterator.next().value).to.be.undefined()
+  })
+
+  it('filter', () => {
+    const cache = new Map()
+
+    cache
+      .set('user:1', 'Marcus')
+      .set('user:2', 'Norman')
+      .set('user:3', 'Christian')
+      .set('user:4', 'Supercharge')
+
+    const users = cache.filter((key, value) => {
+      return key === 'user:1' || value === 'Norman'
+    })
+
+    expect(users.size()).to.equal(2)
+    expect(users.has('user:1')).to.be.true()
+    expect(users.has('user:2')).to.be.true()
+    expect(users.has('user:3')).to.be.false()
+    expect(users.has('user:4')).to.be.false()
+  })
+
+  it('for..of', () => {
+    const iterable = Map.of([
+      [1, 'Marcus'],
+      [2, 'Supercharge']
+    ])
+
+    const array = []
+
+    for (const [key, value] of iterable) {
+      array.push({ [key]: value })
+    }
+
+    expect(array).to.equal([
+      { 1: 'Marcus' },
+      { 2: 'Supercharge' }
+    ])
+  })
+
+  it('Symbol.iterator', () => {
+    const map = Map.of([
+      [1, 'Marcus'],
+      [2, 'Supercharge']
+    ])
+
+    const iterable = map[Symbol.iterator]()
+    expect(iterable.next).to.be.a.function()
+
+    const array = []
+
+    for (const [key, value] of iterable) {
+      array.push({ [key]: value })
+    }
+
+    expect(array).to.equal([
+      { 1: 'Marcus' },
+      { 2: 'Supercharge' }
     ])
   })
 })
